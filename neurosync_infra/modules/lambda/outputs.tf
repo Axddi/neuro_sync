cd25225f92540612b58049a93f6fc3116ad86fdc
iramk596 <iram.khan7321@gmail.com>
2026-03-24 07:54:07 +0530

    module_lambda_invoke_added

--- neurosync_infra/modules/lambda/outputs.tf
+++ neurosync_infra/modules/lambda/outputs.tf
@@ -5,3 +5,7 @@ output "lambda_function_name" {
 output "lambda_function_arn" {
   value = aws_lambda_function.neurosync_lambda.arn
 }
+
+output "lambda_invoke_arn" {
+  value = aws_lambda_function.neurosync_lambda.invoke_arn
+}
